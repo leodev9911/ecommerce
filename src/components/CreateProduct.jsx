512867b958ccb41ffffa2aa9eab7d4f0fde137e8
Leonardo Fernández <leonardo99112821385@gmail.com>
2023-11-19 22:50:56 -0500

    We can post products now, hell yea

--- src/components/CreateProduct.jsx
+++ src/components/CreateProduct.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import axios from 'axios'
 
 export default function CreateProduct () {
   const [formData, setFormData] = useState({
@@ -7,6 +8,11 @@ export default function CreateProduct () {
     price: '',
     imageURL: ''
   })
+  const [file, setFile] = useState()
+
+  const tryForm = (event) => {
+    setFile(event.target.files)
+  }
 
   const handleChange = (event) => {
     const { value, name } = event.target
@@ -21,21 +27,61 @@ export default function CreateProduct () {
   const handleSubmit = async (event) => {
     event.preventDefault()
 
-    const res = await fetch('https://api.escuelajs.co/api/v1/products/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        title: formData.title,
-        price: formData.price,
-        description: formData.description,
-        categoryId: 1,
-        images: ['https://i.imgur.com/KQc4OZZ.jpeg']
-      })
-    })
+    console.log(file[0])
+    const formData12 = new FormData()
 
-    console.log(res)
+    formData12.append('files', file[0])
+    axios.post('http://localhost:1337/api/upload', formData12)
+      .then((response) => {
+        const imageId = response.data[0].id
+        console.log(imageId)
+        axios.post('http://localhost:1337/api/products', {
+          data: {
+            title: formData.title,
+            description: formData.description,
+            price: formData.price,
+            image: imageId
+          }
+        })
+          .then((response) => console.log(response))
+          .catch((error) => console.error(error))
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+    // const res = await fetch('http://localhost:1337/api/products?populate[image][fields][0]=url&populate[categories][fields][0]=name&populate[subcategories][fields][0]=name', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({
+    //     "data": {
+    //       "title": "Prueba",
+    //       "description": "Probando la api de POST",
+    //       "price": 23,
+    //       // "categories": "Armaduras",
+    //       // "subcategories": "Armaduras 2 en 1"
+    //   }
+    //   })
+    // })
+    // console.log(res.status)
+    // console.log(res)
+
+    // const res = await fetch('https://api.escuelajs.co/api/v1/products/', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({
+    //     title: formData.title,
+    //     price: formData.price,
+    //     description: formData.description,
+    //     categoryId: 1,
+    //     images: ['https://i.imgur.com/KQc4OZZ.jpeg']
+    //   })
+    // })
+
+    // console.log(res)
   }
 
   return (
@@ -63,6 +109,11 @@ export default function CreateProduct () {
         placeholder='imageURL'
         name='imageURL'
         onChange={(event) => handleChange(event)}
+      />
+      <input
+        type='file'
+        name='image'
+        onChange={(event) => tryForm(event)}
       />
       <input type='submit' onClick={(event) => handleSubmit(event)} />
     </form>
